f265a7aa8d94ebbca7a8785a1a8052c3bbc0d59d
Victor <mitchell.victorj@gmail.com>
2024-06-28 15:13:30 +0200

    test

--- master_script.R
+++ master_script.R
@@ -8,7 +8,7 @@ library(tidymodels)
 library(rpart.plot)
 library(caret)
 
-# test
+# test 2
 sports_repo = new("Sports_Repository")
 sports_repo = updateSportsFromAPI(sports_repo, all = "false")
 sports_repo@data$title
