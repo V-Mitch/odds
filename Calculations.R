0b88985b1d7bd56243997240f7e81acae421e125
Victor <mitchell.victorj@gmail.com>
2024-06-30 16:23:08 +0200

    update one extra variable and cleaning

--- Calculations.R
+++ Calculations.R
@@ -12,30 +12,30 @@ fairing_odds <- function(...){
   args <- as.numeric(list(...))
   total_sum <- 0
   for (arg in args){
-      total_sum <- (total_sum + 1/arg)
-    }
-    markup <- total_sum - 1
-    fair_list <- c(rep(NaN,length(args)))
+    total_sum <- (total_sum + 1/arg)
+  }
+  markup <- total_sum - 1
+  fair_list <- c(rep(NaN,length(args)))
   for (i in 1:length(fair_list)){
     fair_list[i] <- (args[[i]]*(1+markup))
   }
-    res = as.data.frame(
-      matrix(
-        c(
-          args, markup,
-          fair_list, 0,
-          1/args, 1-sum(1/args),
-          1/fair_list, 0
-          ),
-        nrow = 4, 
-        byrow = TRUE
-      ) 
-    )
-    rownames(res) <- c("Original", "M_Fair", "Original_Prob", "M_Fair_Prob")
-    colnames(res) <- c(paste("price_", 1:(ncol(res)-1), sep = ""), "markup")
-    return(res)
+  res = as.data.frame(
+    matrix(
+      c(
+        args, markup,
+        fair_list, 0,
+        1/args, 1-sum(1/args),
+        1/fair_list, 0
+      ),
+      nrow = 4, 
+      byrow = TRUE
+    ) 
+  )
+  rownames(res) <- c("Original", "M_Fair", "Original_Prob", "M_Fair_Prob")
+  colnames(res) <- c(paste("price_", 1:(ncol(res)-1), sep = ""), "markup")
+  return(res)
 }
-  
+
 probablize <- function(oddsvector){
   pct1 <- 1/oddsvector[1]
   pct2 <- 1/oddsvector[2]
@@ -46,23 +46,33 @@ probablize <- function(oddsvector){
 calc_row <- function(data){
   
   res <- fairing_odds(data)  
-
+  
   
 }
 
 
-calculate_averages <- function(fixture_dataframe, post_game_varnames, fixture_lookback = 5){
+calculate_averages <- function(fixture_dataframe, 
+                               post_game_varnames, 
+                               fixture_lookback = 5,
+                               weighting = "normal"){
   
   # arrange by date chronologically youngest to oldest game
   fixture_dataframe <- fixture_dataframe %>%
     arrange(desc(fixture.date))
   df_averages <- data.frame(matrix(NA, ncol = length(post_game_varnames)*2, nrow = nrow(fixture_dataframe)))
-  for (i in 1:nrow(fixtures_training)){
-    remaining_fixtures <- fixture_dataframe[i:nrow(fixtures_training),]
+  
+  weights <- switch(weighting,
+                    "normal" = seq(1, length.out = fixture_lookback),
+                    "linear" = seq(1, 0, length.out = fixture_lookback),
+                    "exponential" = exp(seq(-1, -fixture_lookback, 
+                                            length.out = fixture_lookback))
+  )
+  for (i in 1:nrow(fixture_dataframe)){
+    remaining_fixtures <- fixture_dataframe[i:nrow(fixture_dataframe),]
     
-    recent_fixtures <- remaining_fixtures %>%
-      slice(-1) %>% 
-      slice_head(n = fixture_lookback)
+    # recent_fixtures <- remaining_fixtures %>%
+    #   slice(-1) %>% 
+    #   slice_head(n = fixture_lookback)
     current_fixture <- remaining_fixtures %>%
       slice(1)
     
@@ -75,27 +85,90 @@ calculate_averages <- function(fixture_dataframe, post_game_varnames, fixture_lo
       filter(teams.home.name == home_team) %>%
       slice(-1) %>% 
       slice_head(n = fixture_lookback) %>% 
-      summarise(across(all_of(post_game_varnames), ~ mean(., na.rm = TRUE)))
-    # colnames(home_avg_prior_5) <- paste0("avg.prev.5.","home", 
-    #                                      colnames(home_avg_prior_5))
+      summarise(across(all_of(post_game_varnames), ~ weighted.mean(., w = weights, na.rm = TRUE)))
+    
     # Calculate the average of yellow cards for away team when they are the away team
     away_avg_prior_5 <- remaining_fixtures %>%
       filter(teams.away.name == away_team) %>%
       slice(-1) %>% 
       slice_head(n = fixture_lookback) %>% 
-      summarise(across(all_of(post_game_varnames), ~ mean(., na.rm = TRUE)))
-    # colnames(away_avg_prior_5) <- paste0("avg.prev.5.","away", 
-    #                                      colnames(away_avg_prior_5))
+      summarise(across(all_of(post_game_varnames), ~ weighted.mean(., w = weights, na.rm = TRUE)))
     
     df_averages[i,] <- bind_cols(home_avg_prior_5, away_avg_prior_5) %>% 
       suppressMessages()
-
+    
     print(paste0("Calculated averages for fixture ",i,"/",nrow(fixture_dataframe)))
   }
   colnames(df_averages) <- c(paste0("current.team.1.avg.prev.",fixture_lookback,".", post_game_varnames),
                              paste0("current.team.2.avg.prev.",fixture_lookback,".", post_game_varnames))
   return(df_averages)
 }
+
+calculate_time_diff <- function(fixture_dataframe){
+  fixture_dataframe <- fixture_dataframe %>%
+    arrange(fixture.timestamp) %>%  # Make sure data is sorted by timestamp
+    group_by(teams.home.name) %>%  # Group by home team name
+    mutate(
+      teams.home.since.last = fixture.timestamp - lag(fixture.timestamp, default = first(fixture.timestamp)),
+      teams.home.since.last = ifelse(is.na(teams.home.since.last), NaN, teams.home.since.last)
+    ) %>%
+    ungroup() %>%
+    arrange(fixture.timestamp) %>%  # Re-arrange by timestamp again
+    group_by(teams.away.name) %>%  # Group by away team name
+    mutate(
+      teams.away.since.last = fixture.timestamp - lag(fixture.timestamp, default = first(fixture.timestamp)),
+      teams.away.since.last = ifelse(is.na(teams.away.since.last), NaN, teams.away.since.last)
+    ) %>%
+    ungroup()
+  return(fixture_dataframe)
+}
+
+# calculate_recent_games <- function(fixture_dataframe, 
+#                                    days_lookback = 365,
+#                                    weighting = "normal"){
+#   weights <- switch(weighting,
+#                     "normal" = seq(1, length.out = fixture_lookback),
+#                     "linear" = seq(1, 0, length.out = fixture_lookback),
+#                     "exponential" = exp(seq(-1, -fixture_lookback, 
+#                                             length.out = fixture_lookback))
+#   )
+#   for (i in 1:nrow(fixture_dataframe)){
+#     remaining_fixtures <- fixture_dataframe[i:nrow(fixtures_training),]
+#     current_fixture_timestamp <- fixture_dataframe[i, fixture.timestamp]
+#     
+#     recent_fixtures <- remaining_fixtures %>%
+#       slice(-1) %>% 
+#       filter(fixture.timestamp >= current_fixture_timestamp - 
+#                ((24 * 60 * 60) * days_lookback))
+#              current_fixture <- remaining_fixtures %>%
+#                slice(1)
+#              
+#              home_team <- current_fixture["teams.home.name"]
+#              away_team <- current_fixture["teams.away.name"]
+#              
+#              # Calculate the average of yellow cards for home home when they are the home team
+#              home_avg_games <- recent_fixtures %>%
+#                filter(teams.home.name == home_team) %>%
+#                slice(-1) %>% 
+#                
+#              
+#              # Calculate the average of yellow cards for away team when they are the away team
+#              away_avg_games <- remaining_fixtures %>%
+#                filter(teams.away.name == away_team) %>%
+#                slice(-1) %>% 
+#                slice_head(n = fixture_lookback) %>% 
+#                summarise(across(all_of(post_game_varnames), ~ weighted.mean(., w = weights, na.rm = TRUE)))
+#              
+#              df_averages[i,] <- bind_cols(home_avg_prior_5, away_avg_prior_5) %>% 
+#                suppressMessages()
+#              
+#              print(paste0("Calculated averages for fixture ",i,"/",nrow(fixture_dataframe)))
+#   }
+#   colnames(df_averages) <- c(paste0("current.team.1.avg.prev.",fixture_lookback,".", post_game_varnames),
+#                              paste0("current.team.2.avg.prev.",fixture_lookback,".", post_game_varnames))
+#   return(df_averages)
+# }
+
 
 # distinguish the data known post-game
 post_game_varnames <- c("team.1.Shots on Goal", 
@@ -133,6 +206,7 @@ post_game_varnames <- c("team.1.Shots on Goal",
                         "team.2.Red Cards"          ,             
                         "team.2.Total passes"       ,             
                         "team.2.Passes %"           ,
+                        "team.2.goals_prevented"    ,
                         "teams.home.winner"         ,
                         "teams.away.winner"         ,
                         "goals.home"                ,                
@@ -145,3 +219,14 @@ post_game_varnames <- c("team.1.Shots on Goal",
                         "score.extratime.away"      ,                
                         "score.penalty.home"        ,                
                         "score.penalty.away"        )
+
+redundant_vars <- c("fixture.timezone", 
+                    "fixture.venue.id",
+                    "fixture.status.short",
+                    "league.id",
+                    "league.logo",
+                    "league.flag",
+                    "teams.home.id",
+                    "teams.away.id",
+                    "teams.home.logo",
+                    "teams.away.logo")
--- master_script.R
+++ master_script.R
@@ -1,13 +1,19 @@
 # master script for project execution (temporary)
-
+setwd("C:/Users/victo/OneDrive/Spectre/R tests/Betting")
 source("repo_class.R")
 source("DataHandlers.R")
 source("Calculations.R")
 source("football_Repo_2.R")
 library(tidymodels)
+library(ranger)
+library(randomForest)
 library(rpart.plot)
 library(caret)
+library(ggplot2)
+library(reshape2)  
 
+
+# test 3
 sports_repo = new("Sports_Repository")
 sports_repo = updateSportsFromAPI(sports_repo, all = "false")
 sports_repo@data$title
@@ -89,51 +95,91 @@ leagues <- 10
 fixtures_training_2020 <- get_past_matches(leagues, seasons, api_key, api_host, short_limit = 300)
 namefile <- paste0(Sys.Date(), "__", seasons,"_", "leagues")
 save(fixtures_training_2020, file = namefile)
-Sys.sleep(60 + 1)
 
+
+load("2024-06-27__avg_df")
+load("2024-06-28__2020_leagues")
+load("2024-06-27__2021_leagues")
+load("2024-06-27__2022_leagues")
+load("2024-06-27__2023_leagues")
+load("2024-06-27__2024_leagues")
+
+
+# Combine the different years together from the raw datasets
 fixtures_training <- bind_rows(fixtures_training_2020, fixtures_training_2021, fixtures_training_2022, 
-      fixtures_training_2023, fixtures_training_2024) %>% 
-      suppressMessages()
+                               fixtures_training_2023, fixtures_training_2024) %>% 
+  suppressMessages()
 
-
-# convert to numeric for better handling
+# convert some to numeric for better handling
 fixtures_training <- fixtures_training %>% 
   mutate(across(all_of(post_game_varnames), as.numeric))
 
-# function-to-be starts here
 
-avg_df <- calculate_averages(fixtures_training, post_game_varnames, 7)
+# FEATURE ENGINEERING
+#Functions
+avg_df <- calculate_averages(fixtures_training, post_game_varnames, 
+                             fixture_lookback = 15,
+                             weighting = "linear")
+fixtures_training <- calculate_time_diff(fixtures_training)
+games_played_recent <- 
+
 namefile <- paste0(Sys.Date(), "__", "avg_df")
 save(avg_df, file = namefile)
-
 # de-select forward-looking bias variables, and add aggregated post_games
 full_set <- bind_cols(fixtures_training[, !names(fixtures_training) %in% post_game_varnames],avg_df) %>% 
+  select(-redundant_vars) %>% 
   suppressMessages()
-
-
+# setting the output variable
 target_variable <- ifelse(is.na(fixtures_training$teams.home.winner), "draw", 
                           ifelse(fixtures_training$teams.home.winner == 1, "home",
                                  ifelse(is.na(fixtures_training$teams.home.winner), "draw", 
                                         ifelse(fixtures_training$teams.home.winner == 0, "away", NA))))
-    
 full_set$outcome <- target_variable
 full_set$outcome <- as.factor(target_variable)
-# not enough levels for certain predictors
-# full_set <- full_set %>% select(-fixture.referee)
+
+# CLEANING
+# Filter for missing values and correct match status
+# remove full NaN column
+clean_set <- full_set[, colSums(is.na(full_set)) < nrow(full_set)]
+# remove dirty rows with very little data
+clean_set <- clean_set[rowSums(is.na(clean_set)) <= 50, ]
+# clean_set %>%
+#   summarise(across(everything(), ~ sum(is.na(.))))
+# impute the missing averages with a median
+clean_set <- clean_set %>% 
+  mutate(across(where(is.numeric), ~replace_na(., median(., na.rm=TRUE))))
+clean_set[is.na(clean_set)] <- -1
+# only want finished games included - forward-looking variable
+clean_set <- clean_set[clean_set$fixture.status.long == "Match Finished",]
+clean_set <- clean_set %>% select(-c(fixture.status.long, fixture.status.elapsed))
+
 
 # REGRESSION TREE
-tree_spec <- decision_tree() %>% 
-  set_engine("rpart") %>% 
-  set_mode("classification")
+save.image(file = paste0(Sys.Date(),"ready_for_execution.RData"))
+load("2024-06-30ready_for_execution.RData")
+forest_spec <- rand_forest(trees = 20000, 
+                           min_n = 1, 
+                           mtry = 96, 
+                           mode = "classification",
+                           engine = "randomForest") 
 
-split_set <- initial_split(full_set, prop = 0.75, strata =outcome)
+split_set <- initial_split(clean_set, prop = 0.75, strata =outcome)
 train_set <- training(split_set)
 test_set <- testing(split_set)
 
+x_vars <- train_set %>% 
+  select(-outcome)
+y_var <- train_set %>% 
+  select(outcome)
 
+model <- forest_spec %>% 
+  fit_xy(x = x_vars , y = y_var, data = train_set)
 
-model <- tree_spec %>% 
-  fit(formula = outcome ~ ., data = train_set)
+predictions <- predict(model, new_data = test_set)
+
+# Create confusion matrix
+confusionMatrix(predictions$.pred_class, test_set$outcome)
+
+# INTERPRET
 tree_model <- extract_fit_engine(model)
-
-predict(model, new_data = test_set)
+varImpPlot(tree_model)
