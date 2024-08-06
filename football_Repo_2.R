322679401f5cd3832f6466c845dcd906b96fa689
Victor Mitchell <mitchell.victorj@gmail.com>
2024-08-06 15:18:42 +0200

    functionality to pick up on player-specific data

--- football_Repo_2.R
+++ football_Repo_2.R
@@ -67,16 +67,6 @@ get_past_matches <- function(leagues, seasons, api_key, api_host, teams = c(), s
   
   for (i in 1:dim(tib_df)[1]){
     
-    # url <- paste(infinitif,"/fixtures/lineups/?fixture=",tib_df[i,"fixture.id"], sep = "")
-    # res <- GET(url,  add_headers('x-rapidapi-key' = api_key,
-    #                              'x-rapidapi-host' = api_host))
-    # str_res_json <- rawToChar(res$content)
-    # parsed_lineups <- fromJSON(str_res_json)
-    # if (length(parsed_lineups$response) !=0){
-    #   lineup_df <- flatten_to_df_lineup(parsed_lineups$response)
-    # } else{
-    #   lineup_df <- data.frame(matrix(ncol = 0, nrow = 1))
-    # }
     url <- paste(infinitif,"/fixtures/statistics/?fixture=",tib_df[i,"fixture.id"], sep = "")
     res <- GET(url,  add_headers('x-rapidapi-key' = api_key,
                                  'x-rapidapi-host' = api_host))
@@ -93,7 +83,7 @@ get_past_matches <- function(leagues, seasons, api_key, api_host, teams = c(), s
     # final_fixture_row <- cbind(tib_df[i,], lineup_df, stats_df)
     final_fixture_row <- cbind(tib_df[i,], stats_df)
     fixture_df <- bind_rows(final_fixture_row, fixture_df) %>% 
-                  suppressMessages()
+      suppressMessages()
     print(paste0("Fixture ", i ," added to the fixture dataframe. ",res$headers$`x-ratelimit-requests-remaining` ,
                  "/", res$headers$`x-ratelimit-requests-limit`,
                  " tokens remaining from API-Odds. Dimensions of current dataframe: ", 
@@ -126,13 +116,14 @@ get_past_players <- function(leagues, seasons, api_key, api_host, teams = c(), s
   time_loop_start <- Sys.time()
   
   for (i in 1:nrow(tib_df)){
-  # for (i in 1:10){
+    # for (i in 1:10){
     
     # browser()
     
     url <- paste(infinitif,"/fixtures/lineups/?fixture=",tib_df[i,"fixture.id"], sep = "")
     res <- GET(url,  add_headers('x-rapidapi-key' = api_key,
                                  'x-rapidapi-host' = api_host))
+    # obtain the lineup of the players from each fixture 
     str_res_json <- rawToChar(res$content)
     parsed_lineups <- fromJSON(str_res_json)
     if (length(parsed_lineups$response) !=0){
@@ -140,33 +131,31 @@ get_past_players <- function(leagues, seasons, api_key, api_host, teams = c(), s
     } else{
       lineup_df <- data.frame(matrix(ncol = 0, nrow = 1))
     }
+    # obtain the rating of the players during each fixture 
+    url <- paste(infinitif,"/fixtures/players/?fixture=",tib_df[i,"fixture.id"], sep = "")
+    res <- GET(url,  add_headers('x-rapidapi-key' = api_key,
+                                 'x-rapidapi-host' = api_host))
+    str_res_json <- rawToChar(res$content)
+    parsed_lineups <- fromJSON(str_res_json)
+    if (length(parsed_lineups$response) !=0){
+      player_stats_df <- flatten_to_df_rating(parsed_lineups$response)
+    } else{
+      player_stats_df <- data.frame(matrix(ncol = 0, nrow = 1))
+    }
     
+    # add the statistics of each player to the player lineups
+    omit_names <- c("team.id", "team.name", "team.logo", "update", "id", "name", "photo")
+    player_stats_names <- names(player_stats_df)[!names(player_stats_df) %in% omit_names]
+    player_id_cols <- grep("player.id", names(lineup_df), value = TRUE)
+    for (j in 1:length(player_stats_names)){
+      name_of_metric <- player_stats_names[j]
+      for (k in 1:length(player_id_cols)){
+        name_of_column <- gsub("id", name_of_metric, player_id_cols[k])
+        temp_data <- player_stats_df[player_stats_df[,"id"] == as.numeric(lineup_df[,player_id_cols][k]) , ][name_of_metric]
+        lineup_df[name_of_column] <- temp_data
+      }
+    }
     
-    # url <- paste(infinitif,"/fixtures/players/?fixture=",tib_df[i,"fixture.id"], sep = "")
-    # res <- GET(url,  add_headers('x-rapidapi-key' = api_key,
-    #                              'x-rapidapi-host' = api_host))
-    # str_res_json <- rawToChar(res$content)
-    # parsed_lineups <- fromJSON(str_res_json)
-    # if (length(parsed_lineups$response) !=0){
-    #   lineup_df <- flatten_to_df_lineup(parsed_lineups$response)
-    # } else{
-    #   lineup_df <- data.frame(matrix(ncol = 0, nrow = 1))
-    # }
-    
-    # url <- paste(infinitif,"/fixtures/statistics/?fixture=",tib_df[i,"fixture.id"], sep = "")
-    # res <- GET(url,  add_headers('x-rapidapi-key' = api_key,
-    #                              'x-rapidapi-host' = api_host))
-    # str_res_json <- rawToChar(res$content)
-    # parsed_stats <- fromJSON(str_res_json)
-    # if (length(parsed_stats$response) !=0){
-    #   stats_df <- flatten_fixture_stats(parsed_stats$response) %>% select(-logo, -id) %>% t() 
-    #   colnames(stats_df) <- paste(stats_df['name',],stats_df['type',], sep = ".")
-    #   stats_df <- t(stats_df[!rownames(stats_df) %in% c("name", "type"),])
-    # } else {
-    #   stats_df <- data.frame(matrix(ncol = 0, nrow = 1))
-    # }
-    
-    # final_fixture_row <- cbind(tib_df[i,], lineup_df, stats_df)
     final_fixture_row <- cbind(tib_df[i,], lineup_df)
     fixture_df <- bind_rows(final_fixture_row, fixture_df) %>% 
       suppressMessages()
